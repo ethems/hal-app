24f33ffc1f9c733d9339340730d75f719e343948
furkano <Furkan.Ozdeslik@confirmit.com>
2016-12-14 23:29:03 -0800

    Add Logger

---
+++ config/index.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+
+module.exports = () => {
+  const filePath = path.join(__dirname, `./${process.env.NODE_ENV}.json`);
+  try {
+    fs.accessSync(filePath, fs.F_OK);
+  } catch (e) {
+    return process.exit(0);
+  }
+  const configJson = JSON.parse(fs.readFileSync(filePath));
+  const runtimeConfig = {};
+
+  runtimeConfig.serverPort = process.env.PORT || configJson['ServerPort'] || 3000;
+  runtimeConfig.dbUri = configJson.dbUri;
+  runtimeConfig.secret = configJson.Secret;
+
+  return runtimeConfig;
+};
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const logger = require('./lib/logger');
+const server = express();
+
+// ENVIRONMENT
+const environmentTypes = ['production', 'development', 'test'];
+const envArg = process.argv && process.argv[process.argv.length - 1];
+const env = ~environmentTypes.indexOf(envArg) && envArg || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || env;
+// RUNTIME CONFIGIRATION
+const config = require('./config')();
+
+server.listen(config.serverPort, (error) => {
+  if (error) {
+    logger.error(error);
+  } else {
+    logger.info(`===> Listening on port ${config.serverPort} in ${process.env.NODE_ENV} mode`);
+  }
+});
