4248dec611fafc85e1baf87145e5529ca54c0f65
furkano <Furkan.Ozdeslik@confirmit.com>
2016-12-20 09:35:31 -0800

    add more test

--- test/apis/product-controller.test.js
+++ test/apis/product-controller.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const Product = require('../../lib/models/product');
 const co = require('co');
+const should = require('should');
 
 describe('Product Controller', () => {
   let server;
@@ -10,6 +11,57 @@ describe('Product Controller', () => {
   after((done) => {
     server.close();
     done();
+  });
+  describe('PUT', () => {
+    it('should create new product', (done) => {
+      const product = {
+        name: 'product put test'
+      };
+      request(server).put('/api/product').send(product).expect(200).end((err, res) => {
+        should.not.exists(err);
+        res.body.product.name.should.equal('Product Put Test');
+        done();
+      });
+    });
+    it('should respond 500 if there is already a product with same name', (done) => {
+      const product = {
+        name: 'product put test'
+      };
+      request(server).put('/api/product').send(product).expect(500, done);
+    });
+    it('should update a product', (done) => {
+      co(function * () {
+        const product = {
+          name: 'product put test 2'
+        };
+        const productCreated = yield Product.create(product);
+        productCreated.name = 'product put test 2 updated';
+        request(server).put('/api/product').send(productCreated).expect(200).end((err, res) => {
+          should.not.exists(err);
+          res.body.product.name.should.equal('Product Put Test 2 Updated');
+          done();
+        });
+      });
+    });
+  });
+  describe('POST', () => {
+    it('should update price', (done) => {
+      co(function * () {
+        const product = {
+          name: 'product post test'
+        };
+        const productCreated = yield Product.create(product);
+        const price = {
+          price: 23.75
+        };
+        request(server).post(`/api/products/${productCreated.id}/price`).send(price).expect(200).end((err, res) => {
+          const priceHistory = res.body.product.priceHistory;
+          priceHistory.length.should.equal(1);
+          priceHistory[0].price.should.equal(23.75);
+          done();
+        });
+      });
+    });
   });
   describe('GET', () => {
     it('should respond 200 to  /api/products', (done) => {
