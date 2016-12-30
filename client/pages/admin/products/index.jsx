8be34eaa42e40f0b3a836fe0c06507899be88a86
furkano <Furkan.Ozdeslik@confirmit.com>
2016-12-30 10:46:41 -0800

    update ui

--- client/pages/admin/products/index.jsx
+++ client/pages/admin/products/index.jsx
@@ -6,6 +6,7 @@ import './styles/index.scss';
 const Products = props => (
   <div className="products-container">
     <div className="products-actions">
+      <button className="pure-button  button-primary valign-center"><i className="material-icons material-icons.md-18">add</i>New</button>
     </div>
     <div className="products-table-wrapper">
       <ProductsTable/>
